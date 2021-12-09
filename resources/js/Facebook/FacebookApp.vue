6285fa8c2305032286f921f59d7c8d781bb15405
Nezar Fadle <nezarfadle@gmail.com>
2021-12-10 02:25:33 +0800

    Add more variables to facebook module

--- resources/js/Facebook/FacebookApp.vue
+++ resources/js/Facebook/FacebookApp.vue
@@ -1,14 +1,16 @@
 <template>
    <div>
       <h1>Hello VUE Component XXXX</h1>
-      <h1>{{ msg }}</h1>
+      <h1>{{ msg1 }}</h1>
+      <h1>{{ msg2 }}</h1>
    </div>
 </template>
 <script>
 export default { 
    data() {
       return {
-         msg: "This is my message"
+         msg1: "This is my message - 1",
+         msg2: "This is my message - 2"
       }
    }
 }
